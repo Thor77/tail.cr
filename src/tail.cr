9abc39218bb99ac01c9b1128a3f3b4467cf3d228
Bri in The Sky <brianh6854@googlemail.com>
2016-07-22 21:36:20 +0100

    Added Type for delay

--- src/tail.cr
+++ src/tail.cr
@@ -1,12 +1,12 @@
 module Tail
-  def self.tail(target_path : String, delay=1, &block)
+  def self.tail(target_path : String, delay : Number = 1, &block)
     last_line_count = File.read_lines(target_path).size
     loop do
       content = File.read_lines(target_path)
       line_count = content.size
       if line_count > last_line_count
-        new_lines = content[last_line_count..-1]
-        yield new_lines[0]
+        new_lines = content[last_line_count..-1][0]
+        yield new_lines
       end
       last_line_count = line_count
       sleep delay
